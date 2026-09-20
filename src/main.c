fdfcd87e649e50232e07a22cbefffa308260de31
Leo Kelly <kellyl82@tcd.ie>
2026-09-20 22:53:43 +0100

    feat: IPv4 header parsing and IP extraction, also signal interrupt handling

--- src/main.c
+++ src/main.c
@@ -5,6 +5,18 @@
 #include <arpa/inet.h>
 #include <linux/if_ether.h>
 #include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <netinet/ip.h>
+
+// global variable to handle signal interrupts better 
+// (using signal.h's sig_atomic_t to ensure r/w to this glob var wont be interrupted by signal itself)
+volatile sig_atomic_t keep_running = 1;
+// function triggered on ctrl+c interrupt to trip flag
+void intHandler(int dummy){
+    keep_running = 0;
+    printf("\nCaught Ctrl+C interrupt. Shutting down...\n");
+}
 
 int main() {
     // make a new raw socket for ethernet frames
@@ -23,13 +35,15 @@ int main() {
         return 1;
     }
     // otherwise success
-    printf("Raw socket successfully opened! Now listening for packets...\n");
+    printf("Raw socket successfully opened! Now listening for packets... (Ctrl+C to quit)\n");
 
     // allocate buffer to hold packet data (64kb is max possible size for IPv4 or TCP network packet)
     unsigned char buffer[65536];
 
+    // handle user interrupt
+    signal(SIGINT, intHandler);
     // capture loop
-    while(1){
+    while(keep_running){
         /*
         recvfrom (int __fd, void *__restrict __buf, size_t __n, int __flags, __SOCKADDR_ARG __addr, socklen_t *__restrict __addr_len)
         raw_socket holds our file descriptor, we allocated buffer to hold packet data, size is self explanitory. no flags or extra args
@@ -39,11 +53,16 @@ int main() {
         // data_size is -1 for errors, so if < 0 then error occured
         if(data_size < 0){
             // handle error
+            if (errno == EINTR) {
+                // woken up by interrupt signal (say Ctrl+C), break the loop cleanly so we don't accidentally sniff ARP packet
+                break; 
+            }
+            // otherwise failed to recieve packet error
             perror("Failed to recieve.");
             return 1;
         }
         // otherwise success
-        printf("Packet ripped: Size %d bytes\n", data_size);
+        printf("\nPacket ripped: Size %d bytes\n", data_size);
         
         // frame decoding (using ethhdr from if_ether.h, assigned to buffer)
         struct ethhdr *eth = (struct ethhdr *)buffer;
@@ -55,6 +74,23 @@ int main() {
         // print the protocol type in hex (also pulled from packet header, 0x0800 is IPv4)
         // using ntohs (network to host short) to convert big endian (network byte order) to little endian (host byte order)
         printf("Protocol type: 0x%04x\n", ntohs(eth->h_proto));
+
+        // cracking the IP header
+        // check for IPv4 packet
+        if(ntohs(eth->h_proto) == 0x0800){
+            // the ip header is past the 14 bytes of ethernet header (iphdr in netinet/ip.h)
+            struct iphdr *ip = (struct iphdr *)(buffer + sizeof(struct ethhdr));
+
+            // use in.h struct to asign source and dest ip's for parsing with inet_ntoa
+            struct in_addr source_ip;
+            source_ip.s_addr = ip->saddr;
+            struct in_addr dest_ip;
+            dest_ip.s_addr = ip->daddr;
+
+            // print ip's using inet_ntoa from inet.h (converts internet number to ascii representation)
+            printf("Source IP address: %s\n", inet_ntoa(source_ip));
+            printf("Destination IP address: %s\n", inet_ntoa(dest_ip));
+        }
     }
 
     close(raw_socket);
